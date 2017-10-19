ce54d02aee8202322c8f8af834d86c910b732742
Aakash D. Shah <akks.shah1997@gmail.com>
2017-10-19 16:38:28 +0530

    cart page ready
    cart items properly displayed
    known issue : updating qty of items not possible
    removing of items not possible

--- include/functions.php
+++ include/functions.php
@@ -109,32 +109,171 @@
     }
 
     function displayCartItems(){
+      global $con;
+      $ip_add = getIp();
+      $check_pro = "select * from cart where ipadd = '$ip_add'";
+      $run_check = mysqli_query($con, $check_pro);
+      if(mysqli_num_rows($run_check)==0){
+        echo '<div class="heading">';
+        echo '<h3>No items added into the cart!!! Would you like to continue to shop</h3>';
+        echo '<a class="btn btn-default update" href="shop.php">Continue Shopping</a>';
+      echo '</div>';
+      }
+      else{
+        global $con;
+        $ip_add = getIp();
+        $check_pro = "select * from cart where ipadd = '$ip_add'";
+        $run_check = mysqli_query($con, $check_pro);
+        while($row_pro = mysqli_fetch_array($run_check)){
+          $pid = $row_pro['pid'];
+          $qty = $row_pro['qty'];
+          $disp_pro = "select * from products where id = '$pid'";
+          $run_disp = mysqli_query($con,$disp_pro);
+          while($run_disp_pro = mysqli_fetch_array($run_disp)){
+            $id = $run_disp_pro['id']; 
+            $name = $run_disp_pro['name'];
+            $cost = $run_disp_pro['cost']; 
+            $imgurl = $run_disp_pro['imgurl'];
+            $imgurl = "images/final/t/".$imgurl;
+            $total = $qty * $cost;
+            echo '<tr>';
+                echo '<td class="cart_product">';
+                  echo '<a href=""><img  height = "100px"src="'.$imgurl.'" alt=""></a>';
+                echo '</td>';
+                echo '<td class="cart_description">';
+                  echo '<h4><a href="">'.$name.'</a></h4>';
+                  echo "<p>Web ID: HAS$id</p>";
+                echo '</td>';
+                echo '<td class="cart_price">';
+                  echo '<p>'.$cost.'</p>';
+                echo '</td>';
+                echo '<td class="cart_quantity">';
+                  echo '<div class="cart_quantity_button">';
+                    echo '<a class="cart_quantity_up" href=""> + </a>';
+                    echo '<input class="cart_quantity_input" type="text" name="quantity" value="'.$qty.'" autocomplete="off" size="2">';
+                    echo '<a class="cart_quantity_down" href=""> - </a>';
+                  echo '</div>';
+                echo '</td>';
+                echo '<td class="cart_total">';
+                  echo '<p class="cart_total_price">'.$total.'</p>';
+                echo '</td>';
+                echo '<td class="cart_delete">';
+                  echo '<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>';
+                echo '</td>';
+              echo '</tr>';
+          }
+          
+        }
+      }
+    }
 
-      echo '<tr>';
-              echo '<td class="cart_product">';
-                echo '<a href=""><img src="images/cart/one.png" alt=""></a>';
-              echo '</td>';
-              echo '<td class="cart_description">';
-                echo '<h4><a href="">Colorblock Scuba</a></h4>';
-                echo '<p>Web ID: 1089772</p>';
-              echo '</td>';
-              echo '<td class="cart_price">';
-                echo '<p>$59</p>';
-              echo '</td>';
-              echo '<td class="cart_quantity">';
-                echo '<div class="cart_quantity_button">';
-                  echo '<a class="cart_quantity_up" href=""> + </a>';
-                  echo '<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">';
-                  echo '<a class="cart_quantity_down" href=""> - </a>';
-                echo '</div>';
-              echo '</td>';
-              echo '<td class="cart_total">';
-                echo '<p class="cart_total_price">$59</p>';
-              echo '</td>';
-              echo '<td class="cart_delete">';
-                echo '<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>';
-              echo '</td>';
-            echo '</tr>';
-
+    function cartTotalDisplay(){
+      global $con;
+      $ip_add = getIp();
+      $check_pro = "select * from cart where ipadd = '$ip_add'";
+      $run_check = mysqli_query($con, $check_pro);
+      if(mysqli_num_rows($run_check)==0){
+      }
+      else{
+        global $con;
+        $ip_add = getIp();
+        $check_pro = "select * from cart where ipadd = '$ip_add'";
+        $run_check = mysqli_query($con, $check_pro);
+        $total = 0;
+        $tax = 0;
+        $total_cost = 0;
+        while($row_pro = mysqli_fetch_array($run_check)){
+          $pid = $row_pro['pid'];
+          $qty = $row_pro['qty'];
+          $disp_pro = "select * from products where id = '$pid'";
+          $run_disp = mysqli_query($con,$disp_pro);
+          while($run_disp_pro = mysqli_fetch_array($run_disp)){
+            $id = $run_disp_pro['id']; 
+            $name = $run_disp_pro['name'];
+            $cost = $run_disp_pro['cost']; 
+            $imgurl = $run_disp_pro['imgurl'];
+            $imgurl = "images/final/t/".$imgurl;
+            $total += $qty * $cost;
+          }
+        }
+        $tax += $total * 0.28;
+        $total_cost += $total + $tax;
+            echo '<section id="do_action">
+            <div class="container">
+              <div class="heading">
+                <h3>What would you like to do next?</h3>
+                <p>Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.</p>
+              </div>
+              <div class="row">
+                <div class="col-sm-6">
+                  <div class="chose_area">
+                    <ul class="user_option">
+                      <li>
+                        <input type="checkbox">
+                        <label>Use Coupon Code</label>
+                      </li>
+                      <li>
+                        <input type="checkbox">
+                        <label>Use Gift Voucher</label>
+                      </li>
+                      <li>
+                        <input type="checkbox">
+                        <label>Estimate Shipping & Taxes</label>
+                      </li>
+                    </ul>
+                    <ul class="user_info">
+                      <li class="single_field">
+                        <label>Country:</label>
+                        <select>
+                          <option>United States</option>
+                          <option>Bangladesh</option>
+                          <option>UK</option>
+                          <option>India</option>
+                          <option>Pakistan</option>
+                          <option>Ucrane</option>
+                          <option>Canada</option>
+                          <option>Dubai</option>
+                        </select>
+                        
+                      </li>
+                      <li class="single_field">
+                        <label>Region / State:</label>
+                        <select>
+                          <option>Select</option>
+                          <option>Dhaka</option>
+                          <option>London</option>
+                          <option>Dillih</option>
+                          <option>Lahore</option>
+                          <option>Alaska</option>
+                          <option>Canada</option>
+                          <option>Dubai</option>
+                        </select>
+                      
+                      </li>
+                      <li class="single_field zip-field">
+                        <label>Zip Code:</label>
+                        <input type="text">
+                      </li>
+                    </ul>
+                    <a class="btn btn-default update" href="">Get Quotes</a>
+                    <a class="btn btn-default check_out" href="">Continue</a>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="total_area">
+                    <ul>
+                      <li>Cart Sub Total <span>'.$total.'</span></li>
+                      <li>GST (28%) <span>'.$total_cost.'</span></li>
+                      <li>Shipping Cost <span>Free</span></li>
+                      <li>Total <span>'.$total_cost.'</span></li>
+                    </ul>
+                      <a class="btn btn-default update" href="">Update</a>
+                      <a class="btn btn-default check_out" href="">Check Out</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section><!--/#do_action-->';
+      }
     }
 ?>
