d13fe9cf8a7aee845cba5ba8f37b4c03e9a84646
Aakash D. Shah <akks.shah1997@gmail.com>
2017-10-21 04:40:55 +0530

    minor bug fix
    removed the extra update cart button

--- include/functions.php
+++ include/functions.php
@@ -270,8 +270,7 @@
                       <li>Shipping Cost <span>Free</span></li>
                       <li>Total <span>'.$total_cost.'</span></li>
                     </ul>
-                      <a class="btn btn-default update" href="shop.php">Continue Shopping</a>
-                      <input type = "submit" class="btn btn-default update" name="update_cart" value = "Update ?"></button>
+                      <a class="btn btn-default update" href="shop.php">Continue Shopping ?</a>
                       <a class="btn btn-default check_out" href="">Check Out</a>
                   </div>
                 </div>
